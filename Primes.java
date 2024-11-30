0ef1dfa5b513120180a041164c4c0aaad9ffc769
tomer1408 <tomer.rozental@post.runi.ac.il>
2024-11-30 10:25:25 +0200

    homeWork4

--- Primes.java
+++ Primes.java
@@ -21,6 +21,7 @@ public class Primes {
         
     }
      // Count all prime numbers
+     System.out.println("Prime numbers up to " + N + ":");
      for (int i = 2; i <= N; i++) {
         if (numbers[i]) { //  if i is true == prime
             System.out.println(i);
